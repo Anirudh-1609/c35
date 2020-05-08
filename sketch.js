8ce4b0d93401b20a65ebebd07dd11d84bf7cf629
Anirudh-1609 <60318539+Anirudh-1609@users.noreply.github.com>
2020-05-08 18:48:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var gameState=0,playerCount=0,form,player,game;
+var database;
+function setup(){
+    createCanvas(500,500);
+    database=firebase.database();
+  
+}
+
+function draw(){
+    background("white");
+   
+}
+
